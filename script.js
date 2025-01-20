2d991bbe7de67fe7c05814aa1950ba00046c9322
Raonikhil7488 <nikhilrao7488@gmail.com>
2025-01-20 16:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const openModalBtn = document.getElementById('openModal');
 
         // Open modal
         openModalBtn.addEventListener('click', () => {
-            modal.style.display = 'flex';
+            modal.style.display = 'block';
         });
 
         // Close modal when clicking on close button
